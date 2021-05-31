fc6fc2f0a7f59e3315fff2bc6c0d8f8dc2dfcb86
STEFANOS CHATZILEFTHERIS <stefanoschatz13@gmail.com>
2021-05-31 18:49:34 +0300

    NEW PROJECT

---
+++ Rent-car-app/src/rent/car/app/MainApp.java
@@ -0,0 +1,17 @@
+
+package rent.car.app;
+
+/**
+ *
+ * @author Dell
+ */
+public class MainApp {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
